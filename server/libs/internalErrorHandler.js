a5b501f64ddbc6cae6355c62a05beabb0a5fe942
徐怀远 <406892700@qq.com>
2018-06-26 09:51:38 +0800

    提交一些东西

--- server/libs/internalErrorHandler.js
+++ server/libs/internalErrorHandler.js
@@ -3,13 +3,14 @@
  * @Author: Simple
  * @Date: 2018-06-22 14:48:07
  * @Last Modified by: Simple
- * @Last Modified time: 2018-06-22 14:48:29
+ * @Last Modified time: 2018-06-22 20:43:00
  */
 
 module.exports = async (ctx, next) => {
     try {
         await next();
     } catch (err) {
+        ctx.status = 500;
         await ctx.render('error/index', {
             status: 500,
             errmsg: 'Internal Server Error',
